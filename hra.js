9cdd17f312e62c5d47c64fca6194497f93ad335e
Michaela Kotuľová <kotulova.michaela271@gmail.com>
2025-04-17 14:49:40 +0200

    Icon of who is currently playing adjusted

--- hra.js
+++ hra.js
@@ -1,18 +1,28 @@
 let currentPlayer = 'circle';
 let gameButtons = document.querySelectorAll('.game__field--square');
 
+const currentPlayerElement = document.querySelector(
+  '.game__options--state div',
+);
+
 const chosenButton = (event) => {
   const currentButton = event.target;
 
   if (currentPlayer === 'circle') {
     currentButton.classList.add('game__field--square-circle');
-    currentButton.innerHTML = `<img src="circle.svg" alt="Krúžok"></img>`;
+    currentButton.innerHTML = `<img src="circle-black.svg" alt="Krúžok">`;
     currentPlayer = 'cross';
   } else {
     currentButton.classList.add('game__field--square-cross');
-    currentButton.innerHTML = `<img src="cross.svg" alt="Xko"></img>`;
+    currentButton.innerHTML = `<img src="cross-black.svg" alt="Krížik">`;
     currentPlayer = 'circle';
   }
+
+  currentPlayerElement.innerHTML = `
+    <img src="${currentPlayer}.svg" alt="${
+    currentPlayer === 'circle' ? 'Krúžok' : 'Krížik'
+  }">
+  `;
   // Aby sme zamedzili možnosti zmeniť svoj ťah opakovaným kliknutím na ten istý button:
   currentButton.disabled = true;
 };
