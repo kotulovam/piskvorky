3d9baa12e1f397a69aff3d7efb7c847d180d4ffb
Michaela Kotuľová <kotulova.michaela271@gmail.com>
2025-05-18 17:13:36 +0200

    Add counterplayer using API

--- hra.js
+++ hra.js
@@ -1,10 +1,15 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+// import { Winner } from "./winner.js"
 
 let currentPlayer = 'circle';
 const gameButtons = document.querySelectorAll('.game__field--square');
 
-const chosenButton = (event) => {
+const chosenButton = async (event) => {
   const currentButton = event.target;
+
+  gameButtons.forEach((button) => {
+    button.disabled = true
+  })
 
   if (currentPlayer === 'circle') {
     currentButton.classList.add('game__field--square-circle');
@@ -17,12 +22,8 @@ const chosenButton = (event) => {
   }
 
   document.querySelector('.game__options--state div').innerHTML = `
-    <img src="${currentPlayer}.svg" alt="${
-    currentPlayer === 'circle' ? 'Krúžok' : 'Krížik'
-  }">`;
-
-  // Aby sme zamedzili možnosti zmeniť svoj ťah opakovaným kliknutím na ten istý button:
-  currentButton.disabled = true;
+    <img src="${currentPlayer}.svg" alt="${currentPlayer === 'circle' ? 'Krúžok' : 'Krížik'
+    }">`;
 
   let currentField = Array.from(gameButtons);
 
@@ -41,13 +42,39 @@ const chosenButton = (event) => {
     if (winner === 'o' || winner === 'x') {
       const nameofWinner = winner === 'o' ? 'Vyhrálo kolečko' : 'Vyhrál křížek';
       alert(`${nameofWinner}.`);
+      location.reload();
     } else if (winner === 'tie') {
       alert('Hra skončila nerozhodne.');
-    }
-    if (winner) {
       location.reload();
     }
   }, 100);
+
+  if (currentPlayer === "cross") {
+    const response = await fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        board: rewrittenField,
+        player: 'x',
+      }),
+    })
+    const data = await response.json()
+    const { x, y } = data.position
+    const field = gameButtons[x + y * 10]
+    field.disabled = false
+    field.click()
+
+    setTimeout(() => {
+      gameButtons.forEach((button) => {
+        const isTaken =
+          button.classList.contains("game__field--square-circle") ||
+          button.classList.contains("game__field--square-cross");
+        button.disabled = isTaken;
+      });
+    }, 200);
+  }
 };
 
 gameButtons.forEach((button) => {
